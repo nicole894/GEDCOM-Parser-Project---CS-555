03b8e470c67f2a92b027a6b0d3d2a2a6d2e56d05
Namrata Gavhane <49354732+namratadg911@users.noreply.github.com>
2019-10-15 17:09:53 -0400

    Update us21,us22,us08,us09

--- GPtest.py
+++ GPtest.py
@@ -9,7 +9,8 @@ class TestUS(unittest.TestCase):
     __ALL__=["US01","US02","US03","US21","US22","US35","US36","US42",
              "US04","US05","US07","US08","US09","US10","US38","US39"]
     __INFO__=["US35", "US36", "US38", "US39"]
-    p=D.main('GEDCOM_File_withErrors.ged')
+    _path='GEDCOM_File_withErrors.ged'
+    p = D.main(_path)
 
     def run_test(self,_test):
         x = None
@@ -161,6 +162,15 @@ class TestUS(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+        
+
+        
+   
+
+
+
+
 
         
 
